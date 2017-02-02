4920120a742ef52a987defa676ceffb11182ff0a
Yazan Mehyar <stcyazanerror@gmail.com>
2017-02-02 00:01:02 +0000

    V1 almost done - sync & cursor output left

--- src/MC6845.v
+++ src/MC6845.v
@@ -82,42 +82,122 @@ always @ (negedge en) begin
 end
 
 /**************************************************************************************************/
-// output assignment
 
-reg [5:0] scanline_start;
-reg [5:0] scanline_end;
-reg [6:0] current_char_row;
+reg scanline_end;
+reg last_row;
+
+reg [7:0] hz_display_count;
+reg [7:0] hz_syncpos_count;
+reg [3:0] hz_pulsew_count;
+reg [7:0] hz_total_count;
+
+reg [6:0] vt_display_count;
+reg [6:0] vt_syncpos_count;
+reg [3:0] vt_pulsew_count;
+reg [6:0] vt_total_count;
+reg [4:0] vt_fraction_count;
+
+always @ (negedge char_clk) begin
+	if(scanline_end) scanline_end <= 0;
+	else if(~|hz_total_count) scanline_end <= 1;
+end
 
 always @ (negedge char_clk) begin
 	if(~nRESET) begin
-		framestore_adr <= 14'h0000;
-		scanline_start <= 14'h0000;
+		hz_total_count   <= 8'h00;
+		hz_display_count <= 8'h00;
+		hz_syncpos_count <= 8'h00;
+		hz_pulsew_count  <= 4'h0;
+	end else if(scanline_end) begin
+		hz_total_count   <= horz_total;
+		hz_display_count <= horz_display;
+		hz_syncpos_count <= horz_syncpos;
+		hz_pulsew_count  <= horz_pulsew;
 	end else begin
-		if(framestore_adr[7:0] == horz_total) begin // change to reflect +1
-			if()
+		hz_total_count <= hz_total_count + 8'hff;
+		if(|hz_display_count)
+			hz_display_count <= hz_display_count + 8'hff;
+		else if(|hz_syncpos_count)
+			hz_syncpos_count <= hz_syncpos_count + 8'hff;
+		else if(|hz_pulsew_count)
+			hz_pulsew_count  <= hz_pulsew_count  + 4'hf;
+	end
+end
+
+wire next_row = (char_scanline == max_scanline) & scanline_end;
+wire screen_end = last_row & ~|vt_fraction_count & scanline_end; // finish last row
+
+always @ (negedge char_clk) begin
+	if(screen_end)	last_row <= 0;
+	else if(~|vt_total_count & scanline_end) last_row <= 1;
+end
+
+
+
+always @ (negedge char_clk) begin
+	if(~nRESET) begin
+		vt_total_count   <= 7'h00;
+		vt_display_count <= 7'h00;
+		vt_syncpos_count <= 7'h00;
+		vt_fraction_count<= 5'h00;
+		vt_pulsew_count  <= 4'h0;
+	end else if(screen_end) begin
+		vt_total_count   <= vert_total;
+		vt_display_count <= vert_display;
+		vt_syncpos_count <= vert_syncpos;
+		vt_fraction_count<= vert_fraction;
+		vt_pulsew_count  <= vert_pulsew + 4'hf; // pulse from 1 to 16; 16 is 0 -> 15
+	end else if(scanline_end) begin
+		if(next_row) begin
+			vt_total_count <= vt_total_count + 7'hff;
+			if(|vt_display_count)
+				vt_display_count <= vt_display_count + 7'hff;
+			else if(|vt_syncpos_count)
+				vt_syncpos_count <= vt_syncpos_count + 7'hff;
 		end
+		if(|vt_pulsew_count & v_sync) // pulse from 1 to 16
+			vt_pulsew_count <= vt_pulsew_count + 4'hf;
+		if(|vt_fraction_count & last_row)
+			vt_fraction_count <= vt_fraction_count + 5'hff;
+
+	end
+end
+
+/**************************************************************************************************/
+// output assignment
+
+reg [13:0] scanline_start_adr;
+
+always @ (negedge char_clk) begin
+	if(~nRESET) begin
+		framestore_adr		<= 14'h0000;
+		scanline_start_adr	<= 14'h0000;
+	end else if(screen_end) begin
+		framestore_adr		<= start_address;
+		scanline_start_adr	<= start_address;
+	end else if(next_row) begin
+		framestore_adr		<= scanline_start_adr + horz_display;
+		scanline_start_adr	<= scanline_start_adr + horz_display;
+	end else if(scanline_end) begin
+		framestore_adr		<= scanline_start_adr;
+	end else begin
+		framestore_adr		<= framestore_adr + 14'h0001;
 	end
 end
 
 always @ (negedge char_clk) begin
-
+	if(~nRESET|next_row|screen_end)	char_scanline <= 5'h00;
+	else if(scanline_end)			char_scanline <= char_scanline + 5'h01;
 end
+
+always @ (*) display_en = |hz_display_count & |vt_display_count;
 
 always @ (negedge char_clk) begin
-
+	h_sync;
+	v_sync;
+	cursor;
 end
 
-always @ (negedge char_clk) begin
-
-end
-
-always @ (negedge char_clk) begin
-
-end
-
-always @ (negedge char_clk) begin
-
-end
 
 
 endmodule // MC6845
