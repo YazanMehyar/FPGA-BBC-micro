8293b3f8bd6350ae1297af4bb7e7c53df1e7a308
Yazan Mehyar <yazan.mehyar@student.manchester.ac.uk>
2017-03-24 20:20:13 +0000

    PS2 driver test working

--- tests/PS2Driver_test.v
+++ tests/PS2Driver_test.v
@@ -1,62 +1,57 @@
+`include "TOP.vh"
+
 module PS2Driver_test();
 
 	// input
-	reg CLK;
+	reg clk;
 	reg nRESET;
-	reg En;
-	reg PS2_CLK;
 	reg PS2_DATA;
+	wire PS2_CLK;
+	wire clk_en;
 	
 	// output
 	wire [7:0] DATA;
 	wire DONE;
-	
-	
-	`define CLK_PERIOD 10
+
 	
 	PS2_DRIVER ps2(
-	.CLK(CLK),
+	.clk(clk),
+	.clk_en(clk_en),
 	.nRESET(nRESET),
-	.En(En),
 	.PS2_CLK(PS2_CLK),
 	.PS2_DATA(PS2_DATA),
 	
 	.DATA(DATA),
 	.DONE(DONE));
 	
-initial begin
-	$dumpvars(0, PS2Driver_test);
+	initial $dumpvars(0, PS2Driver_test);
 	
-	CLK = 0;
-	forever #(`CLK_PERIOD/2) CLK = ~CLK;
-	end
+	// Timing
+	initial clk = 0;
+	always #(`CLKPERIOD/2) clk = ~clk;
 	
-	reg [3:0] CLKCOUNTER = 0;
+	reg [6:0] CLKCOUNTER = 0;
 	
-	always @(posedge CLK) CLKCOUNTER <= CLKCOUNTER + 1;
-	always @(posedge CLK) En <= ~|CLKCOUNTER;
+	always @(posedge clk) CLKCOUNTER <= CLKCOUNTER + 1;
+	assign clk_en  = &CLKCOUNTER[2:0];
+	assign PS2_CLK = ~CLKCOUNTER[6];
 	
+	// Test
 	initial begin
-	nRESET <= 0;
-	PS2_CLK <= 1;
-	PS2_DATA<= 1'bx;
-	repeat (10) @(posedge CLK);
+		nRESET <= 0;
+		PS2_DATA<= 1'b1;
+		repeat (5) @(posedge clk);
 	
-	nRESET <= 1;
-	repeat (5) @(posedge En);
+		nRESET <= 1;
+		repeat (5) @(posedge clk);
 	
-	repeat (11) begin
-		while(~CLKCOUNTER[3]) @(posedge CLK);
-		PS2_DATA <= $urandom_range(9,8);
-		PS2_CLK  <= 0;
-		while(CLKCOUNTER[3])  @(negedge CLK);
-		PS2_CLK  <= 1;
+		repeat (33)
+			@(posedge PS2_CLK)
+			PS2_DATA <= $urandom_range(9,8);
+	
+		repeat (5) @(posedge clk);
+		$finish;
 	end
-	
-	repeat (5) @(posedge En);
-	
-	$finish;
-end
 	
 endmodule
 	
